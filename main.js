69685a5be891f42158e5cd19784ec1418b53df28
Daniel Necula <64778947+xopdev@users.noreply.github.com>
2022-10-20 20:58:35 +0200

    Added some comments for me to know what I need to do next. :)

---
+++ main.js
@@ -0,0 +1,8 @@
+// TODO: When the user clicks the "Add" button, get the input value and add a new list to the DOM :
+
+
+// ! 1. Get the input value
+// ! 2. Create a new list
+// ! 3. Add the new list to the DOM
+// ! 4. Clear the input value
+
